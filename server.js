0b015af2c7f1eea0e20b3e3ce251507e6eeab228
Sharif Ahmed <sharif@namespaceit.com>
2021-03-10 12:05:49 +0600

    added route file

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const userRouter = require('./routers/userRouter');
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -12,11 +14,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use('api/users/', userRouter)
+
 app.get('/', (req, res) =>{
     res.json({
         message: "Welcome To Our App"
     })
-})
+});
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
